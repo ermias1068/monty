d8ab75faed92d65327fa34b5f50860d7b4b35de5
ermias1068 <ermiasbeyene@gmail.com>
2023-08-27 11:15:01 -0700

    addding main function

--- main.c
+++ main.c
@@ -1,28 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define STACK_SIZE 100
-
-typedef struct Stack {
-    int data[STACK_SIZE];
-    int top;
-} Stack;
-
-void push(Stack *stack, int value) {
-    if (stack->top < STACK_SIZE) {
-        stack->data[stack->top++] = value;
-    } else {
-        fprintf(stderr, "Error: Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void pall(const Stack *stack) {
-    for (int i = stack->top - 1; i >= 0; i--) {
-        printf("%d\n", stack->data[i]);
-    }
-}
+#include "monty.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
